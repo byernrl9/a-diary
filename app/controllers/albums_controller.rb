eec916dd4313631f944498e858eff84e2b8f7fd5
byernrl9 <12na0ki8@gmail.com>
2022-06-18 12:55:28 +0900

    controllerの設定(create)

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,4 +1,22 @@
 class AlbumsController < ApplicationController
   def index
   end
+
+  def new
+    @album = Album.new
+  end
+
+  def create
+    @album = Album.new(album_params)
+    if @album.save
+      redirect_to albums_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def album_params
+    params.require(:album).permit(:name).merge(user_id: current_user.id)
+  end
 end
