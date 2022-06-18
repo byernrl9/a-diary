bb28cf1ca1e8e1beecf288c4cd628dbce46c2e52
byernrl9 <12na0ki8@gmail.com>
2022-06-18 16:03:15 +0900

    create アクションの定義と処理内容

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -4,6 +4,26 @@ class DiariesController < ApplicationController
   end
 
   def new
+    @album = Album.find(params[:album_id])
     @diary = Diary.new
   end
+
+  def create
+    @lbum = Album.find(params[:album_id])
+    @diary = Diary.new(diary_params)
+    binding.pry
+    if @diary.save 
+     
+      redirect_to album_diaries_path
+    else
+      render :new
+    end
+  end
+
+  private 
+  
+  def diary_params
+    params.require(:diary).permit(:start_time, :weather_id, :image, :title,:content, :place).merge(user_id: current_user.id, album_id: params[:album_id] )
+  end
+
 end
