63468c949084db1d09387b5d2eee38b5c06fafda
byernrl9 <12na0ki8@gmail.com>
2022-06-18 13:56:48 +0900

    newアクションのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#homes'
   resources :albums, only: [:index, :new, :create] do
-    resources :diaries, only: [:index]
+    resources :diaries, only: [:index, :new]
   end
 end
