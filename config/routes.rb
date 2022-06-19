9a35aecd5bcd1e965733d4b6c572880fcab98aa1
byernrl9 <12na0ki8@gmail.com>
2022-06-19 18:30:00 +0900

    diariesのshow ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#homes'
   resources :albums, only: [:index, :new, :create] do
-    resources :diaries, only: [:index, :new, :create]
+    resources :diaries, only: [:index, :new, :create, :show]
   end
 end
