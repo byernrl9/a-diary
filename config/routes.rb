750827125c3b5c5144f597a21a68d84b929584f1
byernrl9 <12na0ki8@gmail.com>
2022-06-20 11:04:38 +0900

    update ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'homes#homes'
-  resources :albums, only: [:index, :new, :create, :edit] do
+  resources :albums, only: [:index, :new, :create, :edit, :update] do
     resources :diaries
   end
 end
