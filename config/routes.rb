61a30f9edbf04537416d881562c543e002d30179
byernrl9 <12na0ki8@gmail.com>
2022-06-20 11:15:10 +0900

    album/destroy ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'homes#homes'
-  resources :albums, only: [:index, :new, :create, :edit, :update] do
+  resources :albums, only: [:index, :new, :create, :edit, :update, :destroy] do
     resources :diaries
   end
 end
