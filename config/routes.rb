e1dce4a9a9a1adbd1f45baad4bdff9569c58c246
byernrl9 <12na0ki8@gmail.com>
2022-06-20 10:47:36 +0900

    album/ edit ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'homes#homes'
-  resources :albums, only: [:index, :new, :create] do
+  resources :albums, only: [:index, :new, :create, :edit] do
     resources :diaries
   end
 end
