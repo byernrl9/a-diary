fc5c0cf400eeafbe581436040687f279faa78ea9
byernrl9 <12na0ki8@gmail.com>
2022-06-21 12:45:20 +0900

    album modelの単体テストコード実行

--- spec/factories/albums.rb
+++ spec/factories/albums.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :album do
-    
+    name             {Faker::Name.initials(number: 2)}
+    association :user
   end
 end
--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Album, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @album = FactoryBot.build(:album)
+  end
+  describe "アルバムの保存" do
+    context "アルバムが保存できる時" do
+      it "アルバム名の情報があれば保存できる" do
+        expect(@album).to be_valid
+      end
+    end
+    context "アルバムが保存できない時" do
+      it "アルバム名が空だと保存できない" do
+        @album.name = ""
+        @album.valid?
+        expect(@album.errors.full_messages).to include("Name can't be blank")
+      end
+      it "ユーザーが紐づいていないと保存できない" do
+        @album.user = nil
+        @album.valid?
+        expect(@album.errors.full_messages).to include("User must exist")
+      end
+    end
+  end
 end
