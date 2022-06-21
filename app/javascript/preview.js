8ec8e1aec3d7f45f0f62444c7b31907c3b1204c8
byernrl9 <12na0ki8@gmail.com>
2022-06-21 21:46:37 +0900

    画像の選択し直しに対応

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,31 +1,21 @@
 document.addEventListener('DOMContentLoaded', function(){
-  // 新規投稿・編集ページのフォームを取得
   const diaryForm = document.getElementById('new_diary');
-  // プレビューを表示するためのスペースを取得
   const previewList = document.getElementById('previews');
-  // 新規投稿・編集ページのフォームがないならここで終了。「!」は論理否定演算子。
   if (!diaryForm) return null;
-  console.log("preview.jsが読み込まれました");
-
-  // input要素を取得
   const fileField = document.querySelector('input[type="file"][name="diary[image]"]');
-  // input要素で値の変化が起きた際に呼び出される関数
   fileField.addEventListener('change', function(e){
-    console.log("input要素で値の変化が起きました");
-    console.log(e.target.files[0]);
+    const alreadyPreview = document.querySelector('.preview');
+    if (alreadyPreview) {
+      alreadyPreview.remove();
+    };
     const file = e.target.files[0];
     const blob = window.URL.createObjectURL(file);
-    console.log(blob);
     const previewWrapper = document.createElement('div');
     previewWrapper.setAttribute('class', 'preview');
-    // 表示する画像を生成
     const previewImage = document.createElement('img');
     previewImage.setAttribute('class', 'preview-image');
     previewImage.setAttribute('src', blob);
-
-     // 生成したHTMLの要素をブラウザに表示させる
-     previewWrapper.appendChild(previewImage);
-     previewList.appendChild(previewWrapper);
-     
+    previewWrapper.appendChild(previewImage);
+    previewList.appendChild(previewWrapper);
   });
 });
