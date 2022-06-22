8a004098cd64b4bc5ffde1bce138984b1433b323
byernrl9 <12na0ki8@gmail.com>
2022-06-22 11:03:07 +0900

    searchアクションのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#homes'
   resources :albums, only: [:index, :new, :create, :edit, :update, :destroy] do
-    resources :diaries
+    resources :diaries do
+      collection do
+        get 'search'
+      end
+    end
   end
 end
